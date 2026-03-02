24f9325fe69a75571f6ee772d010c8e9fdb69680
Tom Maple <tom.maple@outlook.com>
2026-03-02 01:11:59 -0500

    chore: Added the build and publish pipeline.

---
+++ src/Maple.Result.Extensions.HttpClient/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Maple.Result.Extensions.HttpClient
+{
+    public class Class1
+    {
+        public async Task Test()
+        {
+            var httpClient = new System.Net.Http.HttpClient();
+
+            var response = await httpClient.PostAsync("http://test.org", new FormUrlEncodedContent());
+        }
+    }
+}

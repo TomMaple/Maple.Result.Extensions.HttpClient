af8e46333818fefb3da05e9ebae37986ad8b1fc0
Tom Maple <tom.maple@outlook.com>
2026-03-11 00:02:36 -0400

    chore: Removed unused code.

--- src/Maple.Result.Extensions.HttpClient/Mappers/ErrorMapper.cs
+++ src/Maple.Result.Extensions.HttpClient/Mappers/ErrorMapper.cs
@@ -1,78 +1,102 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Maple.Result.Extensions.HttpClient.Helpers;
-using Microsoft.AspNetCore.Mvc;
-using System.Net;
-using Maple.Result.Extensions.HttpClient.Extensions;
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using Maple.Result.Extensions.HttpClient.Helpers;
+//using Microsoft.AspNetCore.Mvc;
+//using System.Net;
+//using Maple.Result.Extensions.HttpClient.Extensions;
 
-namespace Maple.Result.Extensions.HttpClient.Mappers;
+//namespace Maple.Result.Extensions.HttpClient.Mappers;
 
-internal static class ErrorMapper
-{
-    #region consts
+//internal static class ErrorMapper
+//{
+//    #region consts
 
-    private const string DetailTemplatedPropertyName = "detailTemplated";
-    private const string ErrorDetailsPropertyName = "errors";
+//    private const string DetailTemplatedPropertyName = "detailTemplated";
+//    private const string ErrorDetailsPropertyName = "errors";
 
-    #endregion
+//    #endregion
 
-    internal static Error? TryMap(string? content, HttpStatusCode statusCode)
-    {
-        if (string.IsNullOrWhiteSpace(content))
-            return null;
+//    internal static Error Map(HttpStatusCode statusCode, string? reasonPhrase)
+//    {
+//        var typeUri = ErrorUri.None();
+//        var title = ErrorTitleMapper.Map(reasonPhrase, statusCode);
+//        var errorCategory = ErrorCategoryMapper.Map(statusCode);
 
-        var problemDetails = JsonHelper.TryDeserialize<ProblemDetailsInternal>(content);
-        if (problemDetails is null)
-            return null;
+//        var error = errorCategory switch
+//        {
+//            ErrorCategory.Validation => Error.Validation(typeUri, title),
+//            ErrorCategory.Unauthenticated => Error.Unauthenticated(typeUri, title),
+//            ErrorCategory.Unauthorized => Error.Unauthorized(typeUri, title),
+//            ErrorCategory.NotFound => Error.NotFound(typeUri, title),
+//            ErrorCategory.Timeout => Error.Timeout(typeUri, title),
+//            ErrorCategory.Conflict => Error.Conflict(typeUri, title),
+//            ErrorCategory.Failure => Error.Failure(typeUri, title),
+//            ErrorCategory.Critical => Error.Critical(typeUri, title),
+//            ErrorCategory.NotImplemented => Error.NotImplemented(typeUri, title),
+//            ErrorCategory.Unavailable => Error.Unavailable(typeUri, title),
+//            _ => Error.Critical(typeUri, title)
+//        };
 
-        var title = problemDetails.Title;
-        if (string.IsNullOrWhiteSpace(title))
-            return null;
+//        return error;
+//    }
 
-        var typeUri = ErrorUriMapper.MapTypeUri(problemDetails.Type);
-        var instanceUri = ErrorUriMapper.Map(problemDetails.Instance);
-        
-        var detailTemplatedValue = problemDetails.Extensions.GetValueOrNull(DetailTemplatedPropertyName);
-        var detailTemplated = TemplatedMessageMapper.TryMap(detailTemplatedValue);
+//    internal static Error? TryMap(string? content, HttpStatusCode statusCode)
+//    {
+//        if (string.IsNullOrWhiteSpace(content))
+//            return null;
 
-        var errorDetailsValue = problemDetails.Extensions.GetValueOrNull(ErrorDetailsPropertyName);
-        var errorDetails = ErrorDetailsMapper.TryMap(errorDetailsValue);
+//        var problemDetails = JsonHelper.TryDeserialize<ProblemDetailsInternal>(content);
+//        if (problemDetails is null)
+//            return null;
 
-        var errorCategory = ErrorCategoryMapper.Map(statusCode);
+//        var title = problemDetails.Title;
+//        if (string.IsNullOrWhiteSpace(title))
+//            return null;
 
-        var error = errorCategory switch
-        {
-            ErrorCategory.Validation => Error.Validation(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.Unauthenticated => Error.Unauthenticated(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.Unauthorized => Error.Unauthorized(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.NotFound => Error.NotFound(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.Timeout => Error.Timeout(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.Conflict => Error.Conflict(typeUri, title,problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.Failure => Error.Failure(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.Critical => Error.Critical(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.NotImplemented => Error.NotImplemented(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            ErrorCategory.Unavailable => Error.Unavailable(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
-            _ => Error.Critical(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params)
-        };
+//        var typeUri = ErrorUriMapper.MapTypeUri(problemDetails.Type);
+//        var instanceUri = ErrorUriMapper.Map(problemDetails.Instance);
 
-        AddErrorDetails(error, errorDetails);
+//        var detailTemplatedValue = problemDetails.Extensions.GetValueOrNull(DetailTemplatedPropertyName);
+//        var detailTemplated = TemplatedMessageMapper.TryMap(detailTemplatedValue);
 
-        return error;
-    }
+//        var errorDetailsValue = problemDetails.Extensions.GetValueOrNull(ErrorDetailsPropertyName);
+//        var errorDetails = ErrorDetailsMapper.TryMap(errorDetailsValue);
 
-    private static void AddErrorDetails(Error error, IReadOnlyList<ErrorDetail>? errorDetails)
-    {
-        if (errorDetails is not {Count:>0})
-            return;
+//        var errorCategory = ErrorCategoryMapper.Map(statusCode);
 
-        foreach (var errorDetail in errorDetails)
-        {
-            var namedValues = errorDetail.DetailTemplated?.Params
-                                  ?.Select(kv => (kv.Key, kv.Value))
-                                  .ToArray()
-                              ?? [];
+//        var error = errorCategory switch
+//        {
+//            ErrorCategory.Validation => Error.Validation(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.Unauthenticated => Error.Unauthenticated(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.Unauthorized => Error.Unauthorized(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.NotFound => Error.NotFound(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.Timeout => Error.Timeout(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.Conflict => Error.Conflict(typeUri, title,problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.Failure => Error.Failure(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.Critical => Error.Critical(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.NotImplemented => Error.NotImplemented(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            ErrorCategory.Unavailable => Error.Unavailable(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params),
+//            _ => Error.Critical(typeUri, title, problemDetails.Detail, instanceUri, detailTemplated?.TemplateId, detailTemplated?.Params)
+//        };
 
-            error.AddDetail(errorDetail.PropertyPointer, errorDetail.Detail, errorDetail.DetailTemplated?.TemplateId, namedValues);
-        }
-    }
-}
+//        AddErrorDetails(error, errorDetails);
+
+//        return error;
+//    }
+
+//    private static void AddErrorDetails(Error error, IReadOnlyList<ErrorDetail>? errorDetails)
+//    {
+//        if (errorDetails is not {Count:>0})
+//            return;
+
+//        foreach (var errorDetail in errorDetails)
+//        {
+//            var namedValues = errorDetail.DetailTemplated?.Params
+//                                  ?.Select(kv => (kv.Key, kv.Value))
+//                                  .ToArray()
+//                              ?? [];
+
+//            error.AddDetail(errorDetail.PropertyPointer, errorDetail.Detail, errorDetail.DetailTemplated?.TemplateId, namedValues);
+//        }
+//    }
+//}
